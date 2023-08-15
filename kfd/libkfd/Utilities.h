5d5c675ae7926b78318e741e9f30d163abc798fd
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-08-15 05:40:24 -0400

    Update Utilities.h

--- kfd/libkfd/Utilities.h
+++ kfd/libkfd/Utilities.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #import <Foundation/Foundation.h>
 
+uint64_t funVnodeIterateByVnode(uint64_t kfd, uint64_t vnode);
 void funVnodeHide(uint64_t kfd, uint64_t vnode);
 uint64_t kopen(uint64_t puaf_pages, uint64_t puaf_method, uint64_t kread_method, uint64_t kwrite_method);
 void kclose(uint64_t kfd);
