2d0b715d6e21867a121aed2d6d659cd59c4fb518
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-08-15 17:29:24 -0400

    Update Utilities.m

--- kfd/libkfd/Utilities.m
+++ kfd/libkfd/Utilities.m
@@ -8,6 +8,7 @@
 #import <Foundation/Foundation.h>
 #include "Utilities.h"
 #include "libkfd.h"
+#include <sys/stat.h>
 
 //Offsets
 uint32_t off_p_list_le_prev = 0x8;
@@ -367,8 +368,7 @@ BOOL isFileReadable(u64 kfd, NSString* directoryPath, NSString* fileName) {
     return isReadable;
 }
 
-uint64_t funVnodeChown(u64 kfd, NSString* filename, uid_t uid, gid_t gid) {
-    uint64_t vnode = getVnodeAtPathByChdir(kfd, filename.UTF8String);
+uint64_t funVnodeChown(u64 kfd, uint64_t vnode, uid_t uid, gid_t gid) {
     if(vnode == -1) {
         printf("Unable to get vnode, path: %s", filename.UTF8String);
         return -1;
@@ -376,11 +376,8 @@ uint64_t funVnodeChown(u64 kfd, NSString* filename, uid_t uid, gid_t gid) {
     uint64_t v_data = kread64(kfd, vnode + off_vnode_v_data);
     uint32_t v_uid = kread32(kfd, v_data + 0x80);
     uint32_t v_gid = kread32(kfd, v_data + 0x84);    
-    //vnode->v_data->uid
     printf("Patching %s vnode->v_uid %d -> %d\n", filename.UTF8String, v_uid, uid);
     kwrite32(kfd, v_data+0x80, uid);
-    //vnode->v_data->gid
-    printf("Patching %s vnode->v_gid %d -> %d\n", filename.UTF8String, v_gid, gid);
     kwrite32(kfd, v_data+0x84, gid);
     struct stat file_stat;
     if(stat(filename.UTF8String, &file_stat) == 0) {
