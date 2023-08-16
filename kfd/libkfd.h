f9a06aea568f6cf23d5a7389e32b53060a1790ee
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-08-16 14:56:36 -0400

    Update libkfd.h

--- kfd/libkfd.h
+++ kfd/libkfd.h
@@ -205,6 +205,6 @@ void kclose(u64 kfd)
     kfd_free((struct kfd*)(kfd));
 }
 
-unsigned long long testT1SZ_BOOT = 25ULL;
+#define unsigned long long testT1SZ_BOOT = 25ULL;
 
 #endif /* libkfd_h */
