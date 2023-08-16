6590615b5ea0981361cfe6a8b3ea491ee2341f26
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-08-15 23:31:06 -0400

    Update ContentView.swift

--- kfd/ContentView.swift
+++ kfd/ContentView.swift
@@ -42,7 +42,7 @@ struct ContentView: View {
                         kfd = kopen(UInt64(2048), UInt64(1), UInt64(1), UInt64(1))
                         print(dataFromFile(kfd, "/var/containers/Shared/SystemGroup/systemgroup.com.apple.configurationprofiles/Library/ConfigurationProfiles", "CloudConfigurationDetails.plist"))
                         print(dataFromFileCopy(kfd, "/var/containers/Shared/SystemGroup/systemgroup.com.apple.configurationprofiles/Library/ConfigurationProfiles", "CloudConfigurationDetails.plist"))
-                        //testProc(kfd)
+                        test(kfd)
                         let TestPath = "/Applications/AppStore.app"
                         print(FileManager.default.fileExists(atPath: TestPath))
                         funVnodeHide(kfd, getVnodeAtPathByChdir(kfd, TestPath.cString()))
