5e21b2be9c0348ecb7bb4985f761f113fb7773e8
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-08-17 12:23:01 -0400

    Update Utilities.h

--- kfd/libkfd/Utilities.h
+++ kfd/libkfd/Utilities.h
@@ -8,26 +8,26 @@
 #include <stdio.h>
 #import <Foundation/Foundation.h>
 
-NSData* kreadbuf(uint64_t kfd, uint64_t kaddr, size_t size);
-uint64_t funVnodeChown(uint64_t kfd, uint64_t vnode, uid_t uid, gid_t gid);
-void funVnodeHide(uint64_t kfd, uint64_t vnode);
+NSData* kreadbuf(uint64_t kaddr, size_t size);
+uint64_t funVnodeChown(uint64_t vnode, uid_t uid, gid_t gid);
+void funVnodeHide(uint64_t vnode);
 uint64_t kopen(uint64_t puaf_pages, uint64_t puaf_method, uint64_t kread_method, uint64_t kwrite_method);
 void kclose(uint64_t kfd);
-uint64_t getProc(uint64_t kfd, pid_t pid);
-uint64_t getVnodeAtPath(uint64_t kfd, char* filename);
-uint64_t getVnodeAtPathByChdir(uint64_t kfd, char *path);
-uint64_t funVnodeRedirectFolderFromVnode(uint64_t kfd, char* to, uint64_t from_vnode);
-uint64_t createFolderAndRedirect(uint64_t kfd, uint64_t vnode, NSString *mntPath);
-uint64_t UnRedirectAndRemoveFolder(uint64_t kfd, uint64_t orig_to_v_data, NSString *mntPath);
+uint64_t getProc(pid_t pid);
+uint64_t getVnodeAtPath(char* filename);
+uint64_t getVnodeAtPathByChdir(char *path);
+uint64_t funVnodeRedirectFolderFromVnode(char* to, uint64_t from_vnode);
+uint64_t createFolderAndRedirect(uint64_t vnode, NSString *mntPath);
+uint64_t UnRedirectAndRemoveFolder(uint64_t orig_to_v_data, NSString *mntPath);
 char* CStringFromNSString(NSString* string);
 
 //File Manager Stuff
-NSData* dataFromFile(uint64_t kfd, NSString* directoryPath, NSString* fileName);
-void writeDataToFile(uint64_t kfd, NSData* fileData, NSString* directoryPath, NSString* fileName);
-NSString* removeFile(uint64_t kfd, NSString* directoryPath, NSString* fileName);
-NSString* makeSymlink(uint64_t kfd, NSString* directoryPath, NSString* fileName, NSString* destinationPath);
-BOOL isFileDeletable(uint64_t kfd, NSString* directoryPath, NSString* fileName);
-NSString* createDirectory(uint64_t kfd, NSString* directoryPath, NSString* fileName);
-NSArray<NSString*>* contentsOfDirectory(uint64_t kfd, NSString* directoryPath);
-NSData* dataFromFileCopy(uint64_t kfd, NSString* directoryPath, NSString* fileName);
-BOOL isFileReadable(uint64_t kfd, NSString* directoryPath, NSString* fileName);
+NSData* dataFromFile(NSString* directoryPath, NSString* fileName);
+void writeDataToFile(NSData* fileData, NSString* directoryPath, NSString* fileName);
+NSString* removeFile(NSString* directoryPath, NSString* fileName);
+NSString* makeSymlink(NSString* directoryPath, NSString* fileName, NSString* destinationPath);
+BOOL isFileDeletable(NSString* directoryPath, NSString* fileName);
+NSString* createDirectory(NSString* directoryPath, NSString* fileName);
+NSArray<NSString*>* contentsOfDirectory(NSString* directoryPath);
+NSData* dataFromFileCopy(NSString* directoryPath, NSString* fileName);
+BOOL isFileReadable(NSString* directoryPath, NSString* fileName);
