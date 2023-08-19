3a76d15307d1606200e9b615919739fd008b4896
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-08-19 04:36:46 -0400

    Update ContentView.swift

--- kfd/ContentView.swift
+++ kfd/ContentView.swift
@@ -76,6 +76,7 @@ struct ContentView: View {
                             }
                         }
                     } else {
+                        procNameFindOffsets()
                         kclose(kfd)
                         kfd = 0
                     }
