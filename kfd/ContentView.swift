cf3ffdd6d505ec215deb7d50d50bb936f968e7f8
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-08-21 13:24:43 -0400

    Update ContentView.swift

--- kfd/ContentView.swift
+++ kfd/ContentView.swift
@@ -46,6 +46,14 @@ struct ContentView: View {
                 Button {
                     if kfd == 0 {
                         kfd = kopen(UInt64(2048), UInt64(1), UInt64(1), UInt64(1))
+                        //Make /var owned by mobile
+                        funVnodeChown(getVnodeAtPathByChdir("/var".cString()), 501, 501)
+                        //Test Write
+                        writeDataToFile(Data(), "/var", "test.txt")
+                        //Revert /var ownership to mobile:admin
+                        funVnodeChown(getVnodeAtPathByChdir("/var".cString()), 0, 80)
+                        //Test Write 2
+                        writeDataToFile(Data(), "/var", "test2.txt")
                         if !ProfileToRemoveName.isEmpty {
                             print("⬇️ TESTING ⬇️")
                             let ProfilesPath = "/var/containers/Shared/SystemGroup/systemgroup.com.apple.configurationprofiles/Library/ConfigurationProfiles"
