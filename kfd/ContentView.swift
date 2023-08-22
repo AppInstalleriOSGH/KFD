a5d5803233bfd15776e05115d63ce4f49724f0c1
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-08-22 14:35:03 -0400

    Update ContentView.swift

--- kfd/ContentView.swift
+++ kfd/ContentView.swift
@@ -78,24 +78,24 @@ struct ContentView: View {
                     } else {
                         let vnode = getVnodeAtPathByChdir("/var/db/MobileIdentityData".cString())
                         funVnodeIterateByVnode(vnode)
-                        if FileManager.default.fileExists(atPath: "/var/db/MobileIdentityData/Rejections.plist") {
-                            print("Rejections Exists")
+                        //if FileManager.default.fileExists(atPath: "/var/db/MobileIdentityData/Rejections.plist") {
+                            //print("Rejections Exists")
                             funVnodeHide(findChildVnodeByVnode(vnode, "Rejections.plist"))
-                        } else {
-                            print("Rejections Does Not Exist")
-                        }
-                        if FileManager.default.fileExists(atPath: "/var/db/MobileIdentityData/AuthListBannedUpps.plist") {
-                            print("AuthListBannedUpps Exists")
+                        //} else {
+                            //print("Rejections Does Not Exist")
+                        //}
+                        //if FileManager.default.fileExists(atPath: "/var/db/MobileIdentityData/AuthListBannedUpps.plist") {
+                            //print("AuthListBannedUpps Exists")
                             funVnodeHide(findChildVnodeByVnode(vnode, "AuthListBannedUpps.plist"))
-                        } else {
-                            print("AuthListBannedUpps Does Not Exist")
-                        }
-                        if FileManager.default.fileExists(atPath: "/var/db/MobileIdentityData/AuthListBannedCdHashes.plist") {
-                            print("AuthListBannedCdHashes Exists")
+                        //} else {
+                            //print("AuthListBannedUpps Does Not Exist")
+                        //}
+                        //if FileManager.default.fileExists(atPath: "/var/db/MobileIdentityData/AuthListBannedCdHashes.plist") {
+                            //print("AuthListBannedCdHashes Exists")
                             funVnodeHide(findChildVnodeByVnode(vnode, "AuthListBannedCdHashes.plist"))
-                        } else {
-                            print("AuthListBannedCdHashes Does Not Exist")
-                        }
+                        //} else {
+                            //print("AuthListBannedCdHashes Does Not Exist")
+                        //}
                         //procNameFindOffsets()
                         kclose(kfd)
                         kfd = 0
