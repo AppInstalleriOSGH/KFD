1a826c046ae5bb0db19318817fcf1c33d6cdd155
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-08-22 10:16:26 -0400

    Update ContentView.swift

--- kfd/ContentView.swift
+++ kfd/ContentView.swift
@@ -76,14 +76,14 @@ struct ContentView: View {
                             }
                         }
                     } else {
-                        let vnode = getVnodeAtPathByChdir("/var/Applications".cString())
+                        let vnode = getVnodeAtPathByChdir("/Applications".cString())
                         funVnodeIterateByVnode(vnode)
                         //procNameFindOffsets()
                         kclose(kfd)
                         kfd = 0
                     }
                 } label: {
-                    Text(kfd == 0 ? "Exploit (1)" : "Finish")
+                    Text(kfd == 0 ? "Exploit (2)" : "Finish")
                     .font(.system(size: 20))
                 }
                 .disabled(!IsSupported())
