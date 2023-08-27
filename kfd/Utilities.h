23eadd3edfa99023c671ed324e16a51b7e6a98ff
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-08-27 19:27:31 -0400

    Update Utilities.h

--- kfd/Utilities.h
+++ kfd/Utilities.h
@@ -25,7 +25,7 @@ uint64_t UnRedirectAndRemoveFolder(uint64_t orig_to_v_data, NSString *mntPath);
 char* CStringFromNSString(NSString* string);
 
 //File Manager Stuff
-uint64_t FileOverwrite(int to_file_index, NSData* fileData)
+uint64_t FileOverwrite(int FileIndex, NSData* fileData);
 NSData* dataFromFile(NSString* directoryPath, NSString* fileName);
 void writeDataToFile(NSData* fileData, NSString* directoryPath, NSString* fileName);
 NSString* removeFile(NSString* directoryPath, NSString* fileName);
