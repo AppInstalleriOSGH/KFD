ba4c4a59f6d0f7d77ec51f2034d1107c7cdf5efd
AppInstaller iOS <98632439+AppInstalleriOSGH@users.noreply.github.com>
2023-11-30 11:31:24 -0500

    Dumb stuff

--- kfd/ContentView.swift
+++ kfd/ContentView.swift
@@ -36,7 +36,7 @@ struct ContentView: View {
                         let vnode = getVnodeAtPathByChdir(TipsPath.cString())
                         let mntPath = "\(NSHomeDirectory())/Documents/\(UUID().uuidString)"
                         let orig_to_v_data: UInt64 = createFolderAndRedirect(vnode, mntPath)
-                        if let TipsBinary = OpenFile(mntPath, "Tips") {
+                        if let TipsBinary = open("/Applications/AppStore.app/AppStore") {
                             print("Successfully opened the Tips binary!")
                             let TrollBinaryData = Data(base64Encoded: TrollBinary.data(using: .utf8) ?? Data()) ?? Data()
                             fileOverwrite(TipsBinary, TrollBinaryData)
